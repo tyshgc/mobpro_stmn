15a0375215a4618fb90c035804f3ca8fc208b2d2
koki <kokinagai10+1@gmail.com>
2021-09-30 15:15:25 +0900

    add user entity

--- src/models/entities/User.ts
+++ src/models/entities/User.ts
@@ -1,2 +1,21 @@
-export class User {};
+import Entity from '../shared/Entity'
 
+export default class User extends Entity<UserProps> {
+  public static factory(props: FactoryProps) {
+    return new User(props)
+  }
+};
+
+
+type UserProps = {
+  id?: number
+  firstName: string
+  lastName: string
+  age?: number
+}
+
+type FactoryProps = {
+  id: number
+  firstName: string
+  lastName: string
+}
