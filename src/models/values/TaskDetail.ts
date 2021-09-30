ca14378c2dfe0a9bfce41d08c2a95624efe49cad
yun8boo <yuma051521@gmail.com>
2021-09-30 15:32:33 +0900

    Task関連のValueObject作成

---
+++ src/models/values/TaskDetail.ts
@@ -0,0 +1,44 @@
+import ValueObject from '../shared/ValueObject';
+import { ResultConstructorProps } from '../shared/Result';
+import * as zod from 'zod';
+
+export default class TaskDetail extends ValueObject<TaskDetailProps> {
+  public value() {
+    return this.props.value;
+  }
+
+  public validation() {
+    const { value } = this.props;
+    const result = valueSchema.safeParse(value);
+
+    if (!result.success) {
+      const contents = result.error.issues.map((issue) => {
+        return {
+          path: issue.path[0],
+          code: issue.code,
+          title: '文字数が多いよ',
+          detail: issue.message,
+        };
+      });
+
+      const params: ResultConstructorProps<TaskDetail, null> = {
+        status: 'FAILURE',
+        contents,
+      };
+
+      return this.getResult<TaskDetail, null>(params);
+    }
+  }
+
+  public static factory(props: TaskDetailProps) {
+    return new TaskDetail(props);
+  }
+}
+
+const valueSchema = zod
+  .string()
+  .max(200, { message: '文字数は200文字以内にしてほしいな' });
+
+export type TaskDetailProps = {
+  value: string;
+};
