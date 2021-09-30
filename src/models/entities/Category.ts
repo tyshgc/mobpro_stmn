19e096cb1c37b211e50410b3e487bf3c8c1f0cf5
yun8boo <yuma051521@gmail.com>
2021-09-30 15:31:52 +0900

    Category関連作成

--- src/models/entities/Category.ts
+++ src/models/entities/Category.ts
@@ -1,1 +1,36 @@
-export {}
+import Entity from '../shared/Entity';
+import CategoryName, { CategoryNameProps } from '../values/CategoryName';
+
+/**
+ * - カテゴリ名
+ */
+export default class Category extends Entity<CategoryProps> {
+  public getName() {
+    return this.props.name;
+  }
+
+  public changeName(newName: string) {
+    const name = CategoryName.factory({
+      value: newName,
+    });
+
+    this.props = { ...this.props, name };
+  }
+
+  public static factory(props: FactoryProps) {
+    const name = CategoryName.factory({
+      value: props.name.value,
+    });
+
+    return new Category({ name });
+  }
+}
+
+type CategoryProps = {
+  id?: number;
+  name: CategoryName;
+};
+
+type FactoryProps = {
+  name: CategoryNameProps;
+};
