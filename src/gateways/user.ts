be007b751d72e5dedc7eb41052b4cdbf606325c1
tyshgc <hello@tyshgc.pw>
2021-10-06 12:26:48 +0900

    Promiseにした

--- src/gateways/user.ts
+++ src/gateways/user.ts
@@ -1,15 +1,15 @@
 const mockUser: ResponseUserType = {
   id: 1,
-  firstName: '永井',
+  firstName: "永井",
   lastName: "洸気",
-  age: 26
-};
+  age: 26,
+}
 
 export const fetchUser = (): Promise<ResponseUserType> => {
   return new Promise((resolve) => {
     return resolve(mockUser)
   })
-};
+}
 
 export type ResponseUserType = {
   id: number
