5b210642fbc5cc136c2deb3f6d9f37a4262516cb
tyshgc <hello@tyshgc.pw>
2021-10-09 14:01:07 +0900

    add: タスク一覧取得のhooksを雑につくった

---
+++ src/hooks/useTaskList.ts
@@ -0,0 +1,24 @@
+import { useCallback } from "react"
+import { useSelector } from "react-redux"
+import { userAddTask } from "../useCases/todo"
+import { RootState } from "../redux/reducers"
+import store from "../redux"
+
+/**
+ * Task List Hooks
+ * タスクを一覧するHooks
+ */
+export function useTaskList() {
+  const tasks = useSelector((state: RootState) => {
+    return state.todo.tasks
+  })
+
+  const addTask = useCallback(() => {
+    userAddTask(store, { name: "おげ", detail: "とげ" })
+  }, [])
+
+  return {
+    tasks,
+    addTask,
+  }
+}
